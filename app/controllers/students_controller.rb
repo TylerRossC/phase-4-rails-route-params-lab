8dbc2c3f71535d2870865a473287526920fe39c3
tyler <tylercharawell@gmail.com>
2021-07-28 10:04:40 -0700

    commit tree f1cc49d5e22e2bcff29f5f7987579fb2af40970b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,44 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # if params[:first_name].present?
+    #   students = Student.all.find_by(first_name: params[:first_name])
+    #   render json: students
+    # elsif
+    #   params[:last_name].present?
+    #   students = Student.all.find_by(last_name: params[:last_name])
+    #   render json: students
+    # else
+    #   students = Student.all
+    #   render json: students
+    # end
+  end
+
+  def index
+    name = params[:name]
+    students = if name 
+      Student.by_name(name)
+    else
+      Student.all
+    end
     render json: students
+  end
+
+
+  def self.by_name(name)
+    students = self.arel_table
+    self.where((students[:first_name].matches("%#{name}%")).or(students[:last_name].matches("%#{name}%")))
+  end
+
+
+  def show 
+    student = Student.find(params[:id])
+    render json: student 
+  end
+
+  def name_query 
+    student = Student.find(params[:first_name])
+    render json: student
   end
 
 end
